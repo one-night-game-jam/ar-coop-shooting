a4f3a5977e93822938cba8f62c9fd07317155fec
Shota HAMADA <maku693@outlook.com>
2018-02-24 08:34:34 +0900

    Add ability to sync scene's contents via network

---
+++ Assets/Scripts/NetworkingTest/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace NetworkingTest
+{
+	public class  EnemySpawner : NetworkBehaviour
+	{
+		[SerializeField]
+		private GameObject _enemyObject;
+
+		public override void OnStartServer()
+		{
+			var enemy = Instantiate(_enemyObject);
+			NetworkServer.Spawn(enemy);
+		}
+	}
+}
