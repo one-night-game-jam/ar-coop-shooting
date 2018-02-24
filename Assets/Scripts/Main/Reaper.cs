26afeb8132e129a90d48eb0ca3583fd003169884
Shoma SATO <noir.neo.04@gmail.com>
2018-02-24 14:53:23 +0900

    DIE, DIE, DIE!

---
+++ Assets/Scripts/Main/Reaper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+public class Reaper : MonoBehaviour
+{
+    [SerializeField] private float lifetimeSeconds;
+
+    void Start()
+    {
+        TimeOfDeathAsObservable()
+            .Subscribe(_ => Destroy(gameObject))
+            .AddTo(this);
+    }
+
+    private IObservable<long> TimeOfDeathAsObservable()
+    {
+        return Observable.Interval(TimeSpan.FromSeconds(lifetimeSeconds));
+    }
+}
